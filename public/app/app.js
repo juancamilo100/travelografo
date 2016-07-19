4671526cc58d91dc8cbb726809a0acde3dd51057
JuanEspinosa <juan.espinosa2@ge.com>
2016-07-18 22:50:21 -0400

    Map now working

--- public/app/app.js
+++ public/app/app.js
@@ -78,6 +78,35 @@ angular.module('travelografoApp', ['ngMap'])
 
         $scope.markers = [];
         $scope.blogs = [];
-    });
+    })
+    
+    .service('dataService', function ($http, $q) {
 
-require('./scripts/services');
+      this.getMarkers = function(callback) {
+        $http.get('/api/markers').then(callback);
+      };
+
+      this.deleteMarker = function(marker) {
+        console.log("I deleted the " + marker.name + " marker!");
+      };
+
+      this.saveMarkers = function(markers) {
+        var queue = [];
+        markers.forEach(function(marker) {
+            var request;
+            if(!marker._id) {
+              request = $http.post('/api/markers', marker);
+            } else {
+              request = $http.put('/api/markers/' + marker._id, marker).then(function(result) {
+                marker = result.data.marker;
+                return marker;
+              });
+            }
+            queue.push(request);
+        });
+        return $q.all(queue).then(function(results) {
+          console.log("I saved " + markers.length + " markers!");
+        });
+      };
+
+    });
