ffedc6a4157b66b546f970902edb28e96b2a1228
JuanEspinosa <juan.espinosa2@ge.com>
2016-07-22 15:09:12 -0400

    Added blog api path

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,16 @@
 angular.module('travelografoApp', ['ngMap'])
-    .controller('mainCtrl', function(NgMap, $scope, dataService) {
+    .controller('mainCtrl', function(NgMap, $scope, markerDataService, blogDataService) {
 
         function createBlogsFromDatabase() {
-           $scope.markers.forEach(function(marker) {
-            var coordinates = marker.latLng.toString().replace('[', '(').replace(']', ')');
-            console.log("maker coordinates: " + coordinates);
-             createBlog(coordinates);
-           })
+            $scope.markers.forEach(function(marker) {
+                var coordinates = marker.latLng.toString().replace('[', '(').replace(']', ')');
+                console.log("typeof coordinates from database: " + (typeof coordinates));
+                createBlog(coordinates);
+            })
         };
 
         function updateMarkersFromDatabase() {
-            dataService.getMarkers(function(response) {
+            markerDataService.getMarkers(function(response) {
                 var markers = response.data.markers;
 
                 var marker = {
@@ -23,13 +23,14 @@ angular.module('travelografoApp', ['ngMap'])
                     marker.latLng = markers[index].latLng;
                     $scope.markers[index] = marker;
                 });
+                // createBlogsFromDatabase();
             });
         }
 
         updateMarkersFromDatabase();
 
         $scope.saveMarkers = function() {
-            dataService.saveMarkers($scope.markers);
+            markerDataService.saveMarkers($scope.markers);
         };
 
         function createBlog(coordinates) {
@@ -37,7 +38,9 @@ angular.module('travelografoApp', ['ngMap'])
             var blog = {
                 city: "",
                 state: "",
-                country: ""
+                country: "",
+                title: "",
+                body: ""
             }
 
             var geocoder = new google.maps.Geocoder();
@@ -73,12 +76,12 @@ angular.module('travelografoApp', ['ngMap'])
                                 default:
                             }
                         }
-                        // console.log("Blog: " + blog.city);
-                        if(blog.city == "") {
-                          blog.city = "City Not Found"
+                        if (blog.city == "") {
+                            blog.city = "City Not Found"
                         }
+
                         $scope.blogs.push(blog);
-                        console.log("Blogs: " + $scope.blogs);
+                        blogDataService.saveBlogs($scope.blogs);
                     } else {
                         console.log('Location not found');
                     }
@@ -87,6 +90,19 @@ angular.module('travelografoApp', ['ngMap'])
                 }
             })
         };
+
+        $scope.deleteBlog = function(event) {
+
+            var index = $scope.markers.map(function(marker) {
+                return marker.latLng;
+            }).indexOf(event.latLng.toString().replace('(', '[').replace(')', ']'));
+
+            markerDataService.deleteMarker($scope.markers[index], function(response) {
+                console.log("Marker Deleted: " + response.data);
+            })
+            $scope.markers.splice(index, 1);
+            $scope.blogs.splice(index, 1);
+        }
 
         function createMarker(event) {
             // console.log("Create Marker");
@@ -97,7 +113,7 @@ angular.module('travelografoApp', ['ngMap'])
 
             marker.latLng = event.latLng.toString().replace('(', '[').replace(')', ']');
             $scope.markers[$scope.markers.length] = marker;
-            dataService.saveMarkers($scope.markers);
+            markerDataService.saveMarkers($scope.markers);
             updateMarkersFromDatabase();
         }
 
@@ -108,56 +124,89 @@ angular.module('travelografoApp', ['ngMap'])
 
         $scope.deleteMarker = function(event) {
 
-            var index = $scope.markers.map(function(marker) {
-                return marker.latLng;
-            }).indexOf(event.latLng.toString().replace('(', '[').replace(')', ']'));
+            // var index = $scope.markers.map(function(marker) {
+            //     return marker.latLng;
+            // }).indexOf(event.latLng.toString().replace('(', '[').replace(')', ']'));
 
-            dataService.deleteMarker($scope.markers[index], function(response) {
+            markerDataService.deleteMarker($scope.markers[this.index], function(response) {
                 console.log("Marker Deleted: " + response.data);
             })
-            $scope.markers.splice(index, 1);
-            $scope.blogs.splice(index, 1);
+            $scope.markers.splice(this.index, 1);
+            $scope.blogs.splice(this.index, 1);
         }
 
         $scope.markers = [];
         $scope.blogs = [];
 
         $scope.updateMarker = function(event) {
-          var newlatLng = event.latLng.toString().replace('(', '[').replace(')', ']');
-          $scope.markers[this.index].latLng = newlatLng;
-          dataService.saveMarkers($scope.markers);
+            var newlatLng = event.latLng.toString().replace('(', '[').replace(')', ']');
+            $scope.markers[this.index].latLng = newlatLng;
+            markerDataService.saveMarkers($scope.markers);
         }
     })
 
-.service('dataService', function($http, $q) {
-    this.getMarkers = function(callback) {
-        $http.get('/api/markers').then(callback);
-    };
+.service('markerDataService', function($http, $q) {
+        this.getMarkers = function(callback) {
+            $http.get('/api/markers').then(callback);
+        };
 
-    this.deleteMarker = function(marker, callback) {
-        $http.delete('/api/markers/' + marker.id, marker).then(callback);
-    };
+        this.deleteMarker = function(marker, callback) {
+            $http.delete('/api/markers/' + marker.id, marker).then(callback);
+        };
 
-    this.saveMarkers = function(markers) {
-        var queue = [];
-        markers.forEach(function(marker) {
-            var request;
-            if (marker.id == "") {
-                request = $http.post('/api/markers', marker);
-            } else {
-                request = $http.put('/api/markers/' + marker.id, marker).then(function(result) {
-                    marker = result.data.marker;
-                    return marker;
-                });
-            }
-            queue.push(request);
-        });
-        return $q.all(queue).then(function(results) {
-            // console.log("I saved: " + results);
-        });
-    };
+        this.saveMarkers = function(markers) {
+            var queue = [];
+            markers.forEach(function(marker) {
+                var request;
+                if (marker.id == "") {
+                    request = $http.post('/api/markers', marker);
+                } else {
+                    request = $http.put('/api/markers/' + marker.id, marker).then(function(result) {
+                        marker = result.data.marker;
+                        return marker;
+                    });
+                }
+                queue.push(request);
+            });
+            return $q.all(queue).then(function(results) {
+                // console.log("I saved: " + results);
+            });
+        };
 
-    this.updateMarker - function(marker, callback) {
-      $http.put('/api/markers/' + marker.id, marker).then(callback);
-    }
-});
+        this.updateMarker - function(marker, callback) {
+            $http.put('/api/markers/' + marker.id, marker).then(callback);
+        }
+    })
+
+    .service('blogDataService', function($http, $q) {
+        this.getBlogs = function(callback) {
+            $http.get('/api/blogs').then(callback);
+        };
+
+        this.deleteBlog = function(blog, callback) {
+            $http.delete('/api/blogs/' + blog.id, blog).then(callback);
+        };
+
+        this.saveBlogs = function(blogs) {
+            var queue = [];
+            blogs.forEach(function(blog) {
+                var request;
+                if (blog.id == "") {
+                    request = $http.post('/api/blogs', blog);
+                    console.log("Creating new blog");
+                } else {
+                    request = $http.put('/api/blogs/' + blog.id, blog).then(function(result) {
+                        blog = result.data.blog;
+                        console.log("Updated current blog");
+                        return blog;
+                    });
+                }
+                queue.push(request);
+            });
+            return $q.all(queue).then(function(results) {});
+        };
+
+        this.updateBlog - function(blog, callback) {
+            $http.put('/api/blogs/' + blog.id, blog).then(callback);
+        }
+    });
